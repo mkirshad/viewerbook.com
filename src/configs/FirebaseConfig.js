34b994348725b999bb53d19309bc35cb2b147643
Kashif Irshad <kashif.ir@gmail.com>
2021-11-05 22:04:31 +0500

    First Commit for Demo

---
+++ src/configs/FirebaseConfig.js
@@ -0,0 +1,13 @@
+const FirebaseConfig = {
+  apiKey: 'AIzaSyAOWoRQGoegkmwnCca7B2GG5GUG_ZP1lcM',
+  authDomain: 'SK Strategic Soft.firebaseapp.com',
+  databaseURL: 'https://SK Strategic Soft.firebaseio.com',
+  projectId: 'SK Strategic Soft',
+  storageBucket: 'SK Strategic Soft.appspot.com',
+  messagingSenderId: '807555350717',
+  appId: '1:807555350717:web:145ba7c21af47203a2f7d4',
+  measurementId: 'G-8KE7HJB252'
+};
+
+export default FirebaseConfig
+
